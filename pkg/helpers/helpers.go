0320c63ab8ccd497bb493059ad05bb41500b4448
waltwhitezhou <waltwhitezhou@gmail.com>
2022-01-24 16:18:20 +0800

    HTTP 访问日志

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -1,7 +1,14 @@
 // Package helpers 存放辅助方法
 package helpers
 
-import "reflect"
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	mathrand "math/rand"
+	"reflect"
+	"time"
+)
 
 // Empty 类似于 PHP 的 empty() 函数
 func Empty(val interface{}) bool {
@@ -26,4 +33,43 @@ func Empty(val interface{}) bool {
 		return v.IsNil()
 	}
 	return reflect.DeepEqual(val, reflect.Zero(v.Type()).Interface())
+}
+
+// MicrosecondStr 将time.Duration 类型 (nano seconds 为单位)
+// 输出到小数点后 3 位的 ms (microsecond 毫秒，千分之一)
+func MicrosecondStr(elapsed time.Duration) string {
+	return fmt.Sprintf("%.3fms", float64(elapsed.Nanoseconds()))
+}
+
+// RandNumber 生成长度为 length 的随机字符串
+func RandNumber(length int) string {
+	table := []byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+	b := make([]byte, length)
+	n, err := io.ReadAtLeast(rand.Reader, b, length)
+	if n != length {
+		panic(err)
+	}
+	for i := 0; i < len(b); i++ {
+		b[i] = table[int(b[i])%len(table)]
+	}
+	return string(b)
+}
+
+// FirstElement 安全的获取 args[0], 避免panic：runtime error:index out of range
+func FirstElement(args []string) string {
+	if len(args) > 0 {
+		return args[0]
+	}
+	return ""
+}
+
+// RandomString 生成长度为length的随机字符串
+func RandomString(length int) string {
+	mathrand.Seed(time.Now().UnixNano())
+	letters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = letters[mathrand.Intn(len(letters))]
+	}
+	return string(b)
 }
